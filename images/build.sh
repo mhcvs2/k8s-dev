77b0e3177e7f1001faed313ea82ec305138ed307
mhcvs2 <merzhong0501@163.com>
2018-05-28 15:02:00 +0800

    add

--- images/build.sh
+++ images/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t FROM registry.bst-1.cns.bstjpc.com:5000/k8s-dev:v1.10 .
+docker build -t registry.bst-1.cns.bstjpc.com:5000/k8s-dev:v1.10 .
 
 docker push registry.bst-1.cns.bstjpc.com:5000/k8s-dev:v1.10

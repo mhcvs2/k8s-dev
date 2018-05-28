9e95a7a015f36f8826d1af014da1fc70d4725d97
mhcvs2 <merzhong0501@163.com>
2018-05-28 13:43:14 +0800

    add

--- test/list-deploy/main.go
+++ test/list-deploy/main.go
@@ -5,9 +5,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	"fmt"
+	"flag"
 )
 
 func main() {
+	ns := flag.String("ns", "default", "namespace name")
+	flag.Parse()
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
@@ -18,7 +21,7 @@ func main() {
 		panic(err.Error())
 	}
 
-	deployments, err := clientset.AppsV1beta1().Deployments("default").List(metav1.ListOptions{})
+	deployments, err := clientset.AppsV1beta1().Deployments(*ns).List(metav1.ListOptions{})
 	fmt.Println("deployments in namespace default:")
 	for _, deploy := range deployments.Items {
 		fmt.Println(deploy.Name)

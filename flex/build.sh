e692da252f736dcbf1e1a8da86ca8b41a40bcfe9
mhcvs2 <merzhong0501@163.com>
2018-05-29 15:24:34 +0800

    add

--- flex/build.sh
+++ flex/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -f ../Dockerfile_flex --build-arg http_proxy=http://109.105.4.17:3128 --build-arg https_proxy=http://109.105.4.17:3128 \
+cd ..
+docker build -f Dockerfile_flex --build-arg http_proxy=http://109.105.4.17:3128 --build-arg https_proxy=http://109.105.4.17:3128 \
 -t registry.bst-1.cns.bstjpc.com:5000/flex-provisioner:20180529 . \
 && docker push registry.bst-1.cns.bstjpc.com:5000/flex-provisioner:20180529

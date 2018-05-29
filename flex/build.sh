dd9f92316bd6b6a06e6617da7e81e67271b47ad6
mhcvs2 <merzhong0501@163.com>
2018-05-29 15:11:43 +0800

    add

---
+++ flex/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker build --build-arg http_proxy=http://109.105.4.17:3128 --build-arg https_proxy=http://109.105.4.17:3128 \
+-t registry.bst-1.cns.bstjpc.com:5000/flex-provisioner:20180529 . \
+&& docker push registry.bst-1.cns.bstjpc.com:5000/flex-provisioner:20180529

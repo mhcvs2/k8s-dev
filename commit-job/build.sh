f44cff93b3e34e58a19324a303baf7cbeb1ba480
mhcvs2 <merzhong0501@163.com>
2018-05-30 14:39:20 +0800

    add

--- commit-job/build.sh
+++ commit-job/build.sh
@@ -2,6 +2,7 @@
 
 cd ..
 
-docker build -f Dockerfile_commit_job -t registry.bst-1.cns.bstjpc.com:5000/docker-commit-push:20180530 .
+docker build --build-arg http_proxy=http://109.105.4.17:3128 --build-arg https_proxy=http://109.105.4.17:8119 \
+    -f Dockerfile_commit_job -t registry.bst-1.cns.bstjpc.com:5000/docker-commit-push:20180530 .
 
 docker push registry.bst-1.cns.bstjpc.com:5000/docker-commit-push:20180530

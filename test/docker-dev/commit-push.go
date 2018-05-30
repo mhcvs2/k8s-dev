364be379251f8b395f2458adf7e8433d09cbe9aa
mhcvs2 <merzhong0501@163.com>
2018-05-30 13:42:20 +0800

    add

---
+++ test/docker-dev/commit-push.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
+)
+
+func main() {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	commitOptions := types.ContainerCommitOptions{
+		Reference: "new_image:new_tag",
+		Comment: "message",
+		Pause: true,
+		Author: "mhc",
+	}
+
+	containerID := "cf5e220dd86c50548e4488c2cbe947be1abd0076ee095990f9a17567621a7636"
+
+	cli.ContainerCommit(ctx, containerID, commitOptions)
+}
+

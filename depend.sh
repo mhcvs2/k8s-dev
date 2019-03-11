2b3ffeca4f898514bd4f7cfd6f905971241a7430
mahongchao <mahongchao@kingsoft.com>
2019-03-11 20:24:55 +0800

    add

--- depend.sh
+++ depend.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 ds=(
-k8s.io/client-go/...
-k8s.io/apimachinery/...
+k8s.io/client-go
+k8s.io/apimachinery
 github.com/google/gofuzz
-k8s.io/api
 k8s.io/klog
 k8s.io/utils
 k8s.io/code-generator/...
@@ -13,6 +12,12 @@ github.com/golang/glog
 
 
 for i in ${ds[@]}; do
-  echo $i
+  echo "$i---------------------------------------------"
   go get $i
 done
+
+
+echo "cd $GOPATH/src/k8s.io"
+cd $GOPATH/src/k8s.io
+echo "git clone https://github.com/kubernetes/api.git"
+git clone https://github.com/kubernetes/api.git
